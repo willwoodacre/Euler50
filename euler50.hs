1ca8f98379b7b7225e78bcbe076d24c9ea4f98c2
Will Woodacre <wjw15@ic.ac.uk>
2016-07-19 01:13:18 +0100

    Improved method of finding primes for primeList

--- euler50.hs
+++ euler50.hs
@@ -1,28 +1,19 @@
 
-isPrime :: Int -> Bool
-isPrime n
-  = if (n == 0) || (n == 1) then False else isPrime' n (floor (sqrt (fromIntegral n)))
-  where
-    isPrime' :: Int -> Int -> Bool
-    isPrime' prime divisor
-      | divisor == 1             = True
-      | prime `mod` divisor == 0 = False
-      | otherwise                = isPrime' prime (divisor - 1)
-
-nextPrime :: Int -> Int
-nextPrime n
-  | isPrime n' = n'
-  | otherwise  = nextPrime n'
-  where
-    n' = (n + 1)
+isPrime :: [Int] -> Int -> Bool
+isPrime (p : ps) n
+  = p * p > n || (n `rem` p /= 0 && isPrime ps n)
 
 primeList :: [Int]
 primeList
-  = 2 : map (nextPrime) primeList
+  = 2 : 3 : primeList'
+  where
+    primeList' :: [Int]
+    primeList'
+      = filter (isPrime primeList') [5, 7 ..]
 
 problem50 :: [Int] -> (Int, Int) -> (Int, Int)
 problem50 (current : rest) (prime, noPrimes)
-  | current > 100000    = (prime, noPrimes)
+  | current > 10000    = (prime, noPrimes)
   | noPrimes' > noPrimes = problem50 rest (current, noPrimes')
   | otherwise            = problem50 rest (prime, noPrimes)
   where
