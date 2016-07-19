f3086385924615a7684e3b43f21bf2ab30f4bd15
Will Woodacre <wjw15@ic.ac.uk>
2016-07-19 20:10:17 +0100

    Cleaned up problem50 function

--- euler50.hs
+++ euler50.hs
@@ -11,18 +11,22 @@ primeList
     primeList'
       = 3 : filter (isPrime primeList') [5, 7 ..]
 
-problem50 :: [Int] -> (Int, Int) -> (Int, Int)
-problem50 (current : rest) (prime, noPrimes)
-  | current > 1000000    = (prime, noPrimes)
-  | noPrimes' > noPrimes = problem50 rest (current, noPrimes')
-  | otherwise            = problem50 rest (prime, noPrimes)
+problem50 :: (Int, Int)
+problem50
+  = problem50' primeList (2, 1)
   where
-    noPrimes' = findSum current primeList primeList 0 noPrimes
+  problem50' :: [Int] -> (Int, Int) -> (Int, Int)
+  problem50' (current : rest) (prime, noPrimes)
+    | current > 1000000    = (prime, noPrimes)
+    | noPrimes' > noPrimes = problem50' rest (current, noPrimes')
+    | otherwise            = problem50' rest (prime, noPrimes)
+    where
+      noPrimes' = findSum current primeList primeList 0 noPrimes
     
-    findSum :: Int -> [Int] -> [Int] -> Int -> Int -> Int
-    findSum prime (top : tops) (bottom : bottoms) noPrimes curBest
-      | top == prime = 0
-      | prime == 0   = noPrimes     
-      | prime > 0    = findSum (prime - top) tops (bottom : bottoms) (noPrimes + 1) curBest
-      | otherwise    = if curBest > noPrimes then 0 
-                       else findSum (prime + bottom) (top : tops) bottoms (noPrimes - 1) curBest
+      findSum :: Int -> [Int] -> [Int] -> Int -> Int -> Int
+      findSum prime (top : tops) (bottom : bottoms) noPrimes curBest
+        | top == prime = 0
+        | prime == 0   = noPrimes     
+        | prime > 0    = findSum (prime - top) tops (bottom : bottoms) (noPrimes + 1) curBest
+        | otherwise    = if curBest > noPrimes then 0 
+                         else findSum (prime + bottom) (top : tops) bottoms (noPrimes - 1) curBest
