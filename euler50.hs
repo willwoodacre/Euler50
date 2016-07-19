2e371793df69f032bc63d1423ced16af1a4c9099
Will Woodacre <wjw15@ic.ac.uk>
2016-07-19 20:06:44 +0100

    Got findSum to ignore sums that would be less than the current best. Fast enough to get the answer

--- euler50.hs
+++ euler50.hs
@@ -13,15 +13,16 @@ primeList
 
 problem50 :: [Int] -> (Int, Int) -> (Int, Int)
 problem50 (current : rest) (prime, noPrimes)
-  | current > 100000    = (prime, noPrimes)
+  | current > 1000000    = (prime, noPrimes)
   | noPrimes' > noPrimes = problem50 rest (current, noPrimes')
   | otherwise            = problem50 rest (prime, noPrimes)
   where
-    noPrimes' = findSum current primeList primeList 0
+    noPrimes' = findSum current primeList primeList 0 noPrimes
     
-    findSum :: Int -> [Int] -> [Int] -> Int -> Int
-    findSum prime (top : tops) (bottom : bottoms) noPrimes
+    findSum :: Int -> [Int] -> [Int] -> Int -> Int -> Int
+    findSum prime (top : tops) (bottom : bottoms) noPrimes curBest
       | top == prime = 0
-      | prime == 0   = noPrimes
-      | prime > 0    = findSum (prime - top) tops (bottom : bottoms) (noPrimes + 1)
-      | otherwise    = findSum (prime + bottom) (top : tops) bottoms (noPrimes - 1)
+      | prime == 0   = noPrimes     
+      | prime > 0    = findSum (prime - top) tops (bottom : bottoms) (noPrimes + 1) curBest
+      | otherwise    = if curBest > noPrimes then 0 
+                       else findSum (prime + bottom) (top : tops) bottoms (noPrimes - 1) curBest
